ee88a25a540e03e46f58a227f343230c9848fcc6
Zhenpeng Li <zpli21@gmail.com>
2012-09-12 07:02:36 +0000

    a small modification to seqFormat.R
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CorMut@69358 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/seqFormat.R
+++ R/seqFormat.R
@@ -4,15 +4,15 @@ setGeneric("seqFormat",
 		if(formated != "DNAStringSet"){
 			seqAligned <- readDNAMultipleAlignment(filepath=x,format = formated)
 		}else{
-			seqAligned=x
+			seqAligned <- x
 		}
 		#remove the gaps against the reference sequence.
-		allSeq=as.character(seqAligned,use.names=F)
+		allSeq=sapply(as.character(seqAligned,use.names=T),tolower)
 		ref01=s2c(allSeq[1])
 		seq_f01=as.list(sapply(allSeq,function(x)c2s(s2c(x)[ref01!="-"])))
 		seqff01=rawreplace(seq_f01,refseq=seq_f01[[1]])
 		seq_f03=lapply(seqff01,c2s)
-		ReturnSeq=unlist(seq_f03,use.names = F)
+		ReturnSeq=unlist(seq_f03,use.names = T)
 		ReturnSeqf=DNAStringSet(ReturnSeq)
 		return(ReturnSeqf)
 	}
